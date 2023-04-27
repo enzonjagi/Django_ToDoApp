d1f0594b16f8bb89eb0af5ead5f58b770e75f919
enzonjagi <amosndungo@gmail.com>
2023-04-27 11:58:36 +0300

    Turned the Todolist and items into views for user interaction

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,4 +1,12 @@
-#App level url patterns
+"""App level url patterns"""
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("", views.ListListView.as_view(), name="index"),
+    path(
+        "list/<int:list_id>/",
+        views.ItemListView.as_view(),
+        name="list",
+    )
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from .models import ToDoItem, ToDoList
+from django.views.generic import ListView
 
 # Create your views here.
+class ListListView(ListView):
+    """Defines the ToDo List's view
+    
+    Attributes:
+        model: the data-model class to be fetched
+        template_name: the template we'll be formatting the list
+            into a view
+    """
+
+    model = ToDoList
+    template_name = "todo_app/index.html"
+
+class ItemListView(ListView):
+    """Defines the ToDo List Item's View
+    
+    Attributes:
+        model: the todoItem model class to be fetched
+        template_name: where we'll format the model's content
+            into a view
+    Methods:
+        get_query_set: 
+        get_context_data:
+    """
+
+    model = ToDoItem
+    template_name = "todo_app/todo_list.html"
+
+    def get_queryset(self):
+        """
+        Restricts the data returned 
+        to display only the list's to do items
+        """
+        
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+    
+    def get_context_data(self):
+        """
+        Ensures the Todo object is also returned, not just the items
+
+        Also merges the existing data by calling the OG .get_contex_data() 
+        """
+
+        context = super().get_context_data()
+        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+        return context
